ff6ea60d79e34f27d7e93e11bf65effe07da7f6c
favioabrchavez <favioabrchavez@gmail.com>
2026-04-06 16:24:35 -0500

    Add files via upload

--- data.js
+++ data.js
@@ -3,90 +3,81 @@ const TEMPAD_DATA = {
   /* ─────────────────────────────────────────
      ENTRE SEMANA (reunión normal)
   ───────────────────────────────────────── */
-mwb: {
-  nombre: "",
-  programa: [
-    { tipo: "extra",  texto: "♫ 89 <b><i>Jehová bendice al que escucha y obedece</i></b>" },
-    { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-    { tipo: "tdlb",   texto: '1. Si le prestamos atención a Jehová, nos irá bien', mins: 10 },
-    { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
-    { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
-    { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-    { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
-    { tipo: "smm",    texto: "6. Haga revisitas", mins: 5 },
-    { tipo: "extra",  texto: "Consejo", mins: 1 },
-    { tipo: "extra",  texto: "♫ 107 <b><i>Dios nos enseñó a amar</i></b>" },
-    { tipo: "nvc",    texto: "7. Aproveche bien el día más importante del año", mins: 15 },
-    { tipo: "nvc",    texto: "8. Estudio bíblico de la congregación", mins: 30 },
-    { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-    { tipo: "extra",  texto: "♫ 134 <b><i>Los hijos son un regalo de Dios</i></b>" }
-  ]
-},
+  mwb: {
+    nombre: "",
+    programa: [
+      { tipo: "extra",  texto: "♫ 88 <b><i>Hazme conocer tus caminos</i></b>" },
+      { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
+      { tipo: "tdlb",   texto: "1. Escuchemos al que fue instruido por Dios", mins: 10 },
+      { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
+      { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
+      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
+      { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
+      { tipo: "smm",    texto: "6. Explique sus creencias", mins: 5 },
+      { tipo: "extra",  texto: "Consejo", mins: 1 },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>" },
+      { tipo: "nvc",    texto: "7. Necesidades de la congregación", mins: 15 },
+      { tipo: "nvc",    texto: "8. Estudio bíblico de la congregación", mins: 30 },
+      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
+      { tipo: "extra",  texto: "♫ 13 <b><i>Cristo es nuestro modelo</i></b>" }
+    ]
+  },
 
   w: {
     nombre: "",
-    fecha: "21-22 mar",
-    hora: "---",
     programa: [
       { tipo: "extra",  texto: "♫ - <b><i>Canción de inicio</i></b>", editable: true },
       { tipo: "tk",     texto: "<b>Discurso público:</b> -", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ 76 <b><i>Cuéntame lo que sientes</i></b>"},
-      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Sazonemos la verdad con palabras agradables", mins: 60 },
-      { tipo: "extra",  texto: "♫ 160 <b><i>¡Buenas noticias!</i></b>" }
+      { tipo: "extra",  texto: "♫ 82 <b><i>Hagamos que brille nuestra luz</i></b>"},
+      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo ayudar a nuestros familiares no Testigos", mins: 60 },
+      { tipo: "extra",  texto: "♫ 60 <b><i>Hay vidas en juego</i></b>" }
     ]
   },
 
   mwb_sup: {
     nombre: "",
-    fecha: "---",
-    hora: "07:00 p. m.",
     programa: [
-      { tipo: "extra",  texto: "♫ 89 <b><i>Jehová bendice al que escucha y obedece</i></b>" },
-    { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-    { tipo: "tdlb",   texto: '1. Si le prestamos atención a Jehová, nos irá bien', mins: 10 },
-    { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
-    { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
-    { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-    { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
-    { tipo: "smm",    texto: "6. Haga revisitas", mins: 5 },
-    { tipo: "extra",  texto: "Consejo", mins: 1 },
-    { tipo: "extra",  texto: "♫ 107 <b><i>Dios nos enseñó a amar</i></b>" },
-    { tipo: "nvc",    texto: "7. Aproveche bien el día más importante del año", mins: 15 },
-    { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-    { tipo: "tk",    texto: "<b>Discurso de servicio:</b> ¿Qué está dispuesto a hacer “por las buenas noticias”?", mins: 30 },
-    { tipo: "extra",  texto: "♫ --- <b><i>---</i></b>" }
+      { tipo: "extra",  texto: "♫ 88 <b><i>Hazme conocer tus caminos</i></b>" },
+      { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
+      { tipo: "tdlb",   texto: "1. Escuchemos al que fue instruido por Dios", mins: 10 },
+      { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
+      { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
+      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
+      { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
+      { tipo: "smm",    texto: "6. Explique sus creencias", mins: 5 },
+      { tipo: "extra",  texto: "Consejo", mins: 1 },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>" },
+      { tipo: "nvc",    texto: "7. Necesidades de la congregación", mins: 15 },
+      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
+      { tipo: "tk",    texto: "<b>Discurso de servicio:</b> ¿Qué está dispuesto a hacer “por las buenas noticias”?", mins: 30 },
+      { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
     ]
   },
 
   w_sup: {
     nombre: "",
-    fecha: "---",
-    hora: "---",
     programa: [
-      { tipo: "extra", texto: "♫ --- <b><i>---</i></b>", editable: true },
+      { tipo: "extra",  texto: "♫ - <b><i>Canción de inicio</i></b>", editable: true },
       { tipo: "tk", texto: "<b>Discurso público:</b> Cómo cosechar vida eterna", mins: 30, editable: true },
-      { tipo: "extra", texto: "♫ 18 <b><i>Gracias por el rescate</i></b>", editable: true },
-      { tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: ¿Qué hará usted para demostrar que agradece el rescate?", mins: 30 },
+      { tipo: "extra",  texto: "♫ 82 <b><i>Hagamos que brille nuestra luz</i></b>"},
+      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo ayudar a nuestros familiares no Testigos", mins: 30  },
       { tipo: "tk", texto: "<b>Discurso final:</b> Título", mins: 30, editable: true },
-      { tipo: "extra", texto: "♫ --- <b><i>---</i></b>", editable: true }
+      { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
     ]
   },
 
-  otros_1: {
+  CAco1: {
     nombre: "",
-    fecha: "---",
-    hora: "---",
     programa: [
-      { tipo: "extra",  texto: "♫ 25 <b><i>Una posesión especial</i></b>",},
-      { tipo: "tkmi",  texto: "<b>Orador:</b> Demostremos gratitud por lo que Dios y Cristo han hecho por nosotros", mins: 45, editable: true},
-      { tipo: "extra",  texto: "♫ 18 <b><i>Gracias por el rescate</i></b>"}
+      { tipo: "extra",  texto: "9:40 Música", mins: 10},
+      { tipo: "extra",  texto: "9:50 Canción y oración",},
+      { tipo: "CAco1",  texto: "10:00 “El Padre sin duda está buscando a personas así”", mins: 14},
+      { tipo: "",  texto: "Adoremos a Dios “con espíritu” cuando tratemos de entender su guía", mins: 17}
     ]
   },
 
-  otros_2: {
+  CAco2: {
     nombre: "",
-    fecha: "---",
-    hora: "---",
     programa: [
       { tipo: "extra",  texto: "♫ --- <b><i>---</i></b>", editable: true },
       { tipo: "extra",  texto: "Parte 1", mins: 0, editable: true },
