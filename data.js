6a2d27e36080dc182842ba203ab9102d9c56360a
favioabrchavez <favioabrchavez@gmail.com>
2026-04-27 22:23:28 -0500

    Add files via upload

--- data.js
+++ data.js
@@ -1,54 +1,50 @@
 const TEMPAD_DATA = {
 
-  /* ─────────────────────────────────────────
-     ENTRE SEMANA (reunión normal)
-  ───────────────────────────────────────── */
   mwb: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ 86 <b><i>Necesitamos que Jehová nos enseñe</i></b>" },
+      { tipo: "extra",  texto: "♫ 12 <b><i>Jehová, nuestro gran Dios</i></b>" },
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. ¿Cuánto está dispuesto a invertir?", mins: 10 },
+      { tipo: "tdlb",   texto: "1. Felices de tener a Jehová en nuestra vida", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
-      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 2 },
-      { tipo: "smm",    texto: "5. Empiece conversaciones", mins: 2 },
-      { tipo: "smm",    texto: "6. Haga revisitas", mins: 2 },
-      { tipo: "smm",    texto: "7. Discurso", mins: 5 },
-      { tipo: "extra",  texto: "Consejo", mins: 1 },
-      { tipo: "extra",  texto: "♫ 97 <b><i>Nuestra vida depende de la Palabra de Dios</i></b>" },
-      { tipo: "nvc",    texto: "8. Que ningún obstáculo le impida tener su estudio personal", mins: 15 },
-      { tipo: "nvc",    texto: "9. Estudio bíblico de la congregación", mins: 30 },
+      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
+      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 5 },
+      { tipo: "smm",    texto: "6. Haga discípulos", mins: 4 },
+      { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Voy a buscar a los amigos de la paz</i></b>" },
+      { tipo: "nvc",    texto: "7. Nunca deje de hablar de Jehová", mins: 15 },
+      { tipo: "nvc",    texto: "8. Estudio bíblico de la congregación", mins: 30 },
       { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-      { tipo: "extra",  texto: "♫ 70 <b><i>Busquemos a los merecedores</i></b>" }
+      { tipo: "extra",  texto: "♫ 28 <b><i>Cómo hacernos amigos de Jehová</i></b>" }
     ]
   },
 
   w: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ - <b><i>Canción de inicio</i></b>", editable: true },
-      { tipo: "tk",     texto: "<b>Discurso público:</b> -", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ 49 <b><i>Alegremos el corazón de Jehová</i></b>"},
-      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Siga progresando hacia el bautismo", mins: 60 },
-      { tipo: "extra",  texto: "♫ 38 <b><i>Jehová te cuidará</i></b>" }
+      { tipo: "extra",  texto: "INGRESE DATOS<br>♫ - <b><i>Canción de inicio</i></b>", editable: true },
+      { tipo: "tk",     texto: "INGRESE DATOS <br><b>Discurso público:</b> -", mins: 30, editable: true },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>"},
+      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo prepararse para las dificultades que vengan después del bautismo", mins: 60 },
+      { tipo: "extra",  texto: "♫ 154 <b><i>Un amor sin final</i></b>" }
     ]
   },
 
   mwb_sup: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ 88 <b><i>Hazme conocer tus caminos</i></b>" },
+      { tipo: "extra",  texto: "♫ 12 <b><i>Jehová, nuestro gran Dios</i></b>" },
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. Escuchemos al que fue instruido por Dios", mins: 10 },
+      { tipo: "tdlb",   texto: "1. Felices de tener a Jehová en nuestra vida", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
       { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
-      { tipo: "smm",    texto: "6. Explique sus creencias", mins: 5 },
-      { tipo: "extra",  texto: "Consejo", mins: 1 },
-      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>" },
-      { tipo: "nvc",    texto: "7. Necesidades de la congregación", mins: 15 },
+      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 5 },
+      { tipo: "smm",    texto: "6. Haga discípulos", mins: 4 },
+      { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Voy a buscar a los amigos de la paz</i></b>" },
+      { tipo: "nvc",    texto: "7. Nunca deje de hablar de Jehová", mins: 15 },
       { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
       { tipo: "tk",    texto: "<b>Discurso de servicio:</b> ¿Qué está dispuesto a hacer “por las buenas noticias”?", mins: 30 },
       { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
@@ -58,10 +54,10 @@ const TEMPAD_DATA = {
   w_sup: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ - <b><i>Canción de inicio</i></b>", editable: true },
-      { tipo: "tk", texto: "<b>Discurso público:</b> Cómo cosechar vida eterna", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ 82 <b><i>Hagamos que brille nuestra luz</i></b>"},
-      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo ayudar a nuestros familiares no Testigos", mins: 30  },
+      { tipo: "extra",  texto: "INGRESE DATOS<br>♫ - <b><i>Canción de inicio</i></b>", editable: true },
+      { tipo: "tk",     texto: "INGRESE DATOS <br><b>Discurso público:</b> -", mins: 30, editable: true },
+      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>"},
+      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo prepararse para las dificultades que vengan después del bautismo", mins: 60 },
       { tipo: "tk", texto: "<b>Discurso final:</b> Título", mins: 30, editable: true },
       { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
     ]
@@ -70,22 +66,34 @@ const TEMPAD_DATA = {
   CAco1: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "9:40 Música", mins: 10},
-      { tipo: "extra",  texto: "9:50 Canción y oración",},
-      { tipo: "CAco1",  texto: "10:00 “El Padre sin duda está buscando a personas así”", mins: 14},
-      { tipo: "",  texto: "Adoremos a Dios “con espíritu” cuando tratemos de entender su guía", mins: 17}
+      { tipo: "extra", texto: "9:40 Música", mins: 10},
+      { tipo: "extra", texto: "9:50 Canción 85 y oración",},
+      { tipo: "CAco1", texto: "10:00 “El Padre sin duda está buscando a personas así”", mins: 14},
+      { tipo: "",  texto: "<b>SERIE DE DISCURSOS:</b> Adoremos a Dios “con espíritu”...",},
+      { tipo: "",  texto: "· 10:15 ...cuando tratemos de entender su guía", mins: 16},
+      { tipo: "",  texto: "· 10:31 ..cuando estemos desanimados", mins: 15},
+      { tipo: "",  texto: "· 10:47 ...cuando estemos desanimados", mins: 15},
+      { tipo: "extra", texto: "11:05 Canción 88 y anuncios"},
+      { tipo: "", texto: "11:15 ¿Cómo damos a conocer la verdad?", mins: 20},
+      { tipo: "", texto: "11:35 <b>Discurso de bautismo:</b> El significado de su bautismo", mins: 30},
+      { tipo: "extra", texto: "Canción 51"}
     ]
   },
 
   CAco2: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ --- <b><i>---</i></b>", editable: true },
-      { tipo: "extra",  texto: "Parte 1", mins: 0, editable: true },
-      { tipo: "extra",  texto: "Parte 2", mins: 0, editable: true },
-      { tipo: "extra",  texto: "Parte 3", mins: 0, editable: true },
-      { tipo: "extra",  texto: "♫ --- <b><i>---</i></b>", editable: true }
+      { tipo: "extra", texto: "1:20 Música", mins: 10},
+      { tipo: "extra", texto: "1:30 Canción 72 y oración",},
+      { tipo: "CAco1", texto: "1:35 <b>Discurso público:</b> Cómo distinguir lo que está bien de lo que está mal", mins: 30},
+      { tipo: "w", texto: "2:05 <b>Estudio de <i>La Atalaya</i></b>: Cómo distinguir lo que está bien de lo que está mal", mins: 30},
+      { tipo: "extra", texto: "2:35 Canción 56 y anuncios"},
+      { tipo: "",  texto: "<b>SERIE DE DISCURSOS:</b> Adoremos a Dios “con verdad”...",},
+      { tipo: "",  texto: "· 2:45 ...en la familia", mins: 14},
+      { tipo: "",  texto: "· 3:00 ...en un mundo dividido", mins: 14},
+      { tipo: "",  texto: "· 3:15 ...cuando tengamos problemas ecónomicos", mins: 14},
+      { tipo: "", texto: "3:30 “Compra la verdad y nunca la vendas”", mins: 29},
+      { tipo: "extra", texto: "Canción 29 y oración"}
     ]
-  }
-
+  },
 };
