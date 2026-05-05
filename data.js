a8eacc3f150114c957386da3185f8feaece0c53c
favioabrchavez <favioabrchavez@gmail.com>
2026-05-04 21:09:29 -0500

    Add files via upload

--- data.js
+++ data.js
@@ -3,97 +3,68 @@ const TEMPAD_DATA = {
   mwb: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ 12 <b><i>Jehová, nuestro gran Dios</i></b>" },
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. Felices de tener a Jehová en nuestra vida", mins: 10 },
+      { tipo: "tdlb",   texto: "1. Disfrute al máximo de la bendición de Jehová", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
       { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 5 },
-      { tipo: "smm",    texto: "6. Haga discípulos", mins: 4 },
+      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 4 },
+      { tipo: "smm",    texto: "6. Discurso", mins: 5 },
       { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "extra",  texto: "♫ 99 <b><i>Voy a buscar a los amigos de la paz</i></b>" },
-      { tipo: "nvc",    texto: "7. Nunca deje de hablar de Jehová", mins: 15 },
+      { tipo: "nvc",    texto: "7. “Sean siempre hospitalarios”", mins: 15 },
       { tipo: "nvc",    texto: "8. Estudio bíblico de la congregación", mins: 30 },
-      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-      { tipo: "extra",  texto: "♫ 28 <b><i>Cómo hacernos amigos de Jehová</i></b>" }
+      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 }
     ]
   },
 
   w: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "INGRESE DATOS<br>♫ - <b><i>Canción de inicio</i></b>", editable: true },
-      { tipo: "tk",     texto: "INGRESE DATOS <br><b>Discurso público:</b> -", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>"},
-      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo prepararse para las dificultades que vengan después del bautismo", mins: 60 },
-      { tipo: "extra",  texto: "♫ 154 <b><i>Un amor sin final</i></b>" }
+      {tipo: "tk", texto: "<b>Discurso público</b>", mins: 30},
+      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Mejoremos nuestro “arte de enseñar” en la predicación", mins: 60}
     ]
   },
 
   mwb_sup: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "♫ 12 <b><i>Jehová, nuestro gran Dios</i></b>" },
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. Felices de tener a Jehová en nuestra vida", mins: 10 },
+      { tipo: "tdlb",   texto: "1. Disfrute al máximo de la bendición de Jehová", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
       { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 5 },
-      { tipo: "smm",    texto: "6. Haga discípulos", mins: 4 },
+      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 4 },
+      { tipo: "smm",    texto: "6. Discurso", mins: 5 },
       { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "extra",  texto: "♫ 99 <b><i>Voy a buscar a los amigos de la paz</i></b>" },
-      { tipo: "nvc",    texto: "7. Nunca deje de hablar de Jehová", mins: 15 },
+      { tipo: "nvc",    texto: "7. “Sean siempre hospitalarios”", mins: 15 },
       { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-      { tipo: "tk",    texto: "<b>Discurso de servicio:</b> ¿Qué está dispuesto a hacer “por las buenas noticias”?", mins: 30 },
-      { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
+      { tipo: "tk",    texto: "<b>Discurso de servicio</b>", mins: 30 }
     ]
   },
 
   w_sup: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "INGRESE DATOS<br>♫ - <b><i>Canción de inicio</i></b>", editable: true },
-      { tipo: "tk",     texto: "INGRESE DATOS <br><b>Discurso público:</b> -", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ 99 <b><i>Miles de fieles hermanos</i></b>"},
-      { tipo: "w",      texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo prepararse para las dificultades que vengan después del bautismo", mins: 60 },
-      { tipo: "tk", texto: "<b>Discurso final:</b> Título", mins: 30, editable: true },
-      { tipo: "extra",  texto: "♫ - <b><i>Canción final</i></b>", editable: true },
+      {tipo: "tk", texto: "<b>Discurso público</b>", mins: 30},
+      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Mejoremos nuestro “arte de enseñar” en la predicación", mins: 60},
+      {tipo: "tk", texto: "<b>Discurso final</b>"}
     ]
   },
 
-  CAco1: {
+  CO1: {
     nombre: "",
     programa: [
-      { tipo: "extra", texto: "9:40 Música", mins: 10},
-      { tipo: "extra", texto: "9:50 Canción 85 y oración",},
-      { tipo: "CAco1", texto: "10:00 “El Padre sin duda está buscando a personas así”", mins: 14},
-      { tipo: "",  texto: "<b>SERIE DE DISCURSOS:</b> Adoremos a Dios “con espíritu”...",},
-      { tipo: "",  texto: "· 10:15 ...cuando tratemos de entender su guía", mins: 16},
-      { tipo: "",  texto: "· 10:31 ..cuando estemos desanimados", mins: 15},
-      { tipo: "",  texto: "· 10:47 ...cuando estemos desanimados", mins: 15},
-      { tipo: "extra", texto: "11:05 Canción 88 y anuncios"},
-      { tipo: "", texto: "11:15 ¿Cómo damos a conocer la verdad?", mins: 20},
-      { tipo: "", texto: "11:35 <b>Discurso de bautismo:</b> El significado de su bautismo", mins: 30},
-      { tipo: "extra", texto: "Canción 51"}
+      { tipo: "extra", texto: "9:20 Video musical"},
+      { tipo: "extra", texto: "9:30 Canción 160 y oración",},
+      { tipo: "CO1", texto: "9:40 <b>DISCURSO DEL PRESIDENTE DE LA ASAMBLEA:</b> ¿Es realista la idea de ser felices para siempre?", mins: 29},
+      { tipo: "CO1", texto: "10:10 <b>PRODUCCIÓN AUDIOVISUAL:</b> <i>Las buenas noticias según Jesús: Episodio 4<i>", mins: 29}
     ]
   },
 
-  CAco2: {
+  CO2: {
     nombre: "",
     programa: [
-      { tipo: "extra", texto: "1:20 Música", mins: 10},
-      { tipo: "extra", texto: "1:30 Canción 72 y oración",},
-      { tipo: "CAco1", texto: "1:35 <b>Discurso público:</b> Cómo distinguir lo que está bien de lo que está mal", mins: 30},
-      { tipo: "w", texto: "2:05 <b>Estudio de <i>La Atalaya</i></b>: Cómo distinguir lo que está bien de lo que está mal", mins: 30},
-      { tipo: "extra", texto: "2:35 Canción 56 y anuncios"},
-      { tipo: "",  texto: "<b>SERIE DE DISCURSOS:</b> Adoremos a Dios “con verdad”...",},
-      { tipo: "",  texto: "· 2:45 ...en la familia", mins: 14},
-      { tipo: "",  texto: "· 3:00 ...en un mundo dividido", mins: 14},
-      { tipo: "",  texto: "· 3:15 ...cuando tengamos problemas ecónomicos", mins: 14},
-      { tipo: "", texto: "3:30 “Compra la verdad y nunca la vendas”", mins: 29},
-      { tipo: "extra", texto: "Canción 29 y oración"}
+      
     ]
   },
 };
