9c0df81db09f1fa86778f0cfab7eedfd93fba5ec
favioabrchavez <favioabrchavez@gmail.com>
2026-05-12 13:22:51 -0500

    Update program details in data.js

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. Disfrute al máximo de la bendición de Jehová", mins: 10 },
+      { tipo: "tdlb",   texto: "1. “Levántate, oh, mujer, despide luz”", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
       { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 4 },
-      { tipo: "smm",    texto: "6. Discurso", mins: 5 },
+      { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
+      { tipo: "smm",    texto: "6. Haga discípulos", mins: 5 },
       { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "nvc",    texto: "7. “Sean siempre hospitalarios”", mins: 15 },
+      { tipo: "nvc",    texto: "7. Necesidades de la congregación", mins: 15 },
       { tipo: "nvc",    texto: "8. Estudio bíblico de la congregación", mins: 30 },
       { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 }
     ]
@@ -21,7 +21,7 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "tk", texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Mejoremos nuestro “arte de enseñar” en la predicación", mins: 60}
+      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Confiemos en el soberano del universo", mins: 60}
     ]
   },
 
@@ -29,15 +29,14 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. Disfrute al máximo de la bendición de Jehová", mins: 10 },
+      { tipo: "tdlb",   texto: "1. “Levántate, oh, mujer, despide luz”", mins: 10 },
       { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
       { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
       { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Explique sus creencias", mins: 4 },
-      { tipo: "smm",    texto: "6. Discurso", mins: 5 },
+      { tipo: "smm",    texto: "5. Haga revisitas", mins: 4 },
+      { tipo: "smm",    texto: "6. Haga discípulos", mins: 5 },
       { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "nvc",    texto: "7. “Sean siempre hospitalarios”", mins: 15 },
-      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
+      { tipo: "nvc",    texto: "7. Necesidades de la congregación", mins: 15 },
       { tipo: "tk",    texto: "<b>Discurso de servicio</b>", mins: 30 }
     ]
   },
@@ -46,7 +45,7 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "tk", texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Mejoremos nuestro “arte de enseñar” en la predicación", mins: 60},
+      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Confiemos en el soberano del universo", mins: 60},
       {tipo: "tk", texto: "<b>Discurso final</b>"}
     ]
   },
@@ -54,10 +53,16 @@ const TEMPAD_DATA = {
   CO1: {
     nombre: "",
     programa: [
-      { tipo: "extra", texto: "9:20 Video musical"},
-      { tipo: "extra", texto: "9:30 Canción 160 y oración",},
-      { tipo: "CO1", texto: "9:40 <b>DISCURSO DEL PRESIDENTE DE LA ASAMBLEA:</b> ¿Es realista la idea de ser felices para siempre?", mins: 29},
-      { tipo: "CO1", texto: "10:10 <b>PRODUCCIÓN AUDIOVISUAL:</b> <i>Las buenas noticias según Jesús: Episodio 4<i>", mins: 29}
+      {tipo: "extra", texto: "9:20 Video musical"},
+      {tipo: "extra", texto: "9:30 Canción 160 y oración"},
+      {tipo: "CO1", texto: "9:40 <b>DISCURSO DEL PRESIDENTE DE LA ASAMBLEA:</b> ¿Es realista la idea de ser felices para siempre?", mins: 29},
+      {tipo: "CO1", texto: "10:10 <b>PRODUCCIÓN AUDIOVISUAL:</b> <i>Las buenas noticias según Jesús: Episodio 4<i>", mins: 29},
+      {tipo: "extra", texto: "11:05 Canción 7 y anuncios"},
+      {tipo: "CO1", texto: "11:15 SERIE DE DISCURSOS: ¡Las profecías sobre el Mesías se cumplieron!"},
+      {tipo: "", texto: "• “Él mismo tomó nuestras enfermedades”"},
+      {tipo: "", texto: "• “No discutirá”"},
+      {tipo: "", texto: "• “Nunca les hablaba sin utilizar alguna comparación”"},
+      {tipo: "extra", texto: "12:10 Canción 14 e intermedio"}
     ]
   },
 
