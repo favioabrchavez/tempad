43d368b765542f392049171edce7cc753eec106a
favioabrchavez <favioabrchavez@gmail.com>
2026-06-03 13:49:32 -0500

    Add files via upload

--- data.js
+++ data.js
@@ -3,53 +3,53 @@ const TEMPAD_DATA = {
   mwb: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. ¡Cuánto amamos nuestro paraíso espiritual!", mins: 10 },
-      { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
-      { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
-      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Empiece conversaciones", mins: 2 },
-      { tipo: "smm",    texto: "6. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "7. Explique sus creencias", mins: 3 },
-      { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "nvc",    texto: "8. ¿Tendrás tú una vida llena de cosas buenas?", mins: 15 },
-      { tipo: "nvc",    texto: "9. Estudio bíblico de la congregación", mins: 30 },
-      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 }
+      {tipo: "extra",texto: "Palabras de introducción", mins: 1},
+      {tipo: "tdlb", texto: "1. “No te dejes intimidar [...], porque ‘yo estoy contigo’”", mins: 10},
+      {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
+      {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
+      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
+      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
+      {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
+      {tipo: "nvc",  texto: "7. ¡Sé valiente como Jeremías!", mins: 6},
+      {tipo: "nvc",  texto: "8. “Listos para presentar una defensa [...] con apacibilidad y profundo respeto”", mins: 9},
+      {tipo: "nvc",  texto: "8. Estudio bíblico de la congregación", mins: 30},
+      {tipo: "extra",texto: "Palabras de conclusión", mins: 3}
     ]
   },
 
   w: {
     nombre: "",
     programa: [
-      {tipo: "tk", texto: "Discurso público", mins: 30},
-      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Sea perspicaz y “tendrá éxito”", mins: 60}
+      {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Por qué somos felices aunque nos odien", mins: 60}
     ]
   },
 
   mwb_sup: {
     nombre: "",
     programa: [
-      { tipo: "extra",  texto: "Palabras de introducción", mins: 1 },
-      { tipo: "tdlb",   texto: "1. ¡Cuánto amamos nuestro paraíso espiritual!", mins: 10 },
-      { tipo: "tdlb",   texto: "2. Busquemos perlas escondidas", mins: 10 },
-      { tipo: "tdlb",   texto: "3. Lectura de la Biblia", mins: 4 },
-      { tipo: "smm",    texto: "4. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "5. Empiece conversaciones", mins: 2 },
-      { tipo: "smm",    texto: "6. Empiece conversaciones", mins: 3 },
-      { tipo: "smm",    texto: "7. Explique sus creencias", mins: 3 },
-      { tipo: "extra",  texto: "Consejo para estudiantes", mins: 1 },
-      { tipo: "nvc",    texto: "8. ¿Tendrás tú una vida llena de cosas buenas?", mins: 15 },
-      { tipo: "extra",  texto: "Palabras de conclusión", mins: 3 },
-      { tipo: "tk",    texto: "Discurso de servicio", mins: 30 }
+      {tipo: "extra",texto: "Palabras de introducción", mins: 1},
+      {tipo: "tdlb", texto: "1. “No te dejes intimidar [...], porque ‘yo estoy contigo’”", mins: 10},
+      {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
+      {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
+      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
+      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
+      {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
+      {tipo: "nvc",  texto: "7. ¡Sé valiente como Jeremías!", mins: 6},
+      {tipo: "nvc",  texto: "8. “Listos para presentar una defensa [...] con apacibilidad y profundo respeto”", mins: 9},
+      {tipo: "extra",texto: "Palabras de conclusión", mins: 3},
+      {tipo: "tk",   texto: "<b>Discurso de servicio</b>", mins: 30 }
     ]
   },
 
   w_sup: {
     nombre: "",
     programa: [
-      {tipo: "tk", texto: "Discurso público", mins: 30},
-      {tipo: "w", texto: "<b>Estudio de <i>La Atalaya</i></b>: Sea perspicaz y “tendrá éxito”", mins: 30},
-      {tipo: "tk", texto: "Discurso final", mins: 30}
+      {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Por qué somos felices aunque nos odien", mins: 60},
+      {tipo: "tk",  texto: "<b>Discurso final</b>", mins: 30}
     ]
   },
 
@@ -57,15 +57,9 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "extra", texto: "9:20 Video musical"},
-      {tipo: "extra", texto: "9:30 Canción 160 y oración"},
-      {tipo: "CO1", texto: "9:40 <b>DISCURSO DEL PRESIDENTE DE LA ASAMBLEA:</b> ¿Es realista la idea de ser felices para siempre?", mins: 29},
-      {tipo: "CO1", texto: "10:10 <b>PRODUCCIÓN AUDIOVISUAL:</b> <i>Las buenas noticias según Jesús: Episodio 4<i>", mins: 29},
-      {tipo: "extra", texto: "11:05 Canción 7 y anuncios"},
-      {tipo: "CO1", texto: "11:15 SERIE DE DISCURSOS: ¡Las profecías sobre el Mesías se cumplieron!"},
-      {tipo: "", texto: "• “Él mismo tomó nuestras enfermedades”"},
-      {tipo: "", texto: "• “No discutirá”"},
-      {tipo: "", texto: "• “Nunca les hablaba sin utilizar alguna comparación”"},
-      {tipo: "extra", texto: "12:10 Canción 14 e intermedio"}
+      {tipo: "extra", texto: "9:30 Canción 160 y oración",},
+      {tipo: "CO1",   texto: "9:40 <b>DISCURSO DEL PRESIDENTE DE LA ASAMBLEA:</b> ¿Es realista la idea de ser felices para siempre?", mins: 29},
+      {tipo: "CO1",   texto: "10:10 <b>PRODUCCIÓN AUDIOVISUAL:</b> <i>Las buenas noticias según Jesús: Episodio 4<i>", mins: 29}
     ]
   },
 
