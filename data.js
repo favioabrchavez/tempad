ef6812a560eb818e7ba2be15db802c2290fcdd86
favioabrchavez <favioabrchavez@gmail.com>
2026-06-15 21:53:43 -0500

    Update program content and timings in data.js

--- data.js
+++ data.js
@@ -4,15 +4,15 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "extra",texto: "Palabras de introducción", mins: 1},
-      {tipo: "tdlb", texto: "1. No nos enfermemos espiritualmente como les pasó a los de Judá", mins: 10},
+      {tipo: "tdlb", texto: "1. No respetaron el templo de Jehová", mins: 10},
       {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
       {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
-      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 2},
-      {tipo: "smm",  texto: "5. Empiece conversaciones", mins: 2},
-      {tipo: "smm",  texto: "6. Haga revisitas", mins: 4},
-      {tipo: "smm",  texto: "7. Explique sus creencias", mins: 3},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
+      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
+      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
       {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
-      {tipo: "nvc",  texto: "8. Proteja su corazón de la información falsa", mins: 8},
+      {tipo: "nvc",  texto: "7. ¿Cómo podemos demostrar que respetamos el Salón del Reino?", mins: 5},
+      {tipo: "nvc",  texto: "8. Cómo usamos las donaciones: Mantenemos nuestros Salones del Reino en buen estado", mins: 10},
       {tipo: "nvc",  texto: "9. Necesidades de la congregación", mins: 7},
       {tipo: "nvc",  texto: "10. Estudio bíblico de la congregación", mins: 30},
       {tipo: "extra",texto: "Palabras de conclusión", mins: 3}
@@ -23,7 +23,7 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Nada puede impedir que “el Dios de la verdad” cumpla su propósito", mins: 60}
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo mantener la perspectiva correcta ante las dificultades", mins: 60}
     ]
   },
 
@@ -31,15 +31,15 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "extra",texto: "Palabras de introducción", mins: 1},
-      {tipo: "tdlb", texto: "1. No nos enfermemos espiritualmente como les pasó a los de Judá", mins: 10},
+      {tipo: "tdlb", texto: "1. No respetaron el templo de Jehová", mins: 10},
       {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
       {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
-      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 2},
-      {tipo: "smm",  texto: "5. Empiece conversaciones", mins: 2},
-      {tipo: "smm",  texto: "6. Haga revisitas", mins: 4},
-      {tipo: "smm",  texto: "7. Explique sus creencias", mins: 3},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
+      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
+      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
       {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
-      {tipo: "nvc",  texto: "8. Proteja su corazón de la información falsa", mins: 8},
+      {tipo: "nvc",  texto: "7. ¿Cómo podemos demostrar que respetamos el Salón del Reino?", mins: 5},
+      {tipo: "nvc",  texto: "8. Cómo usamos las donaciones: Mantenemos nuestros Salones del Reino en buen estado", mins: 10},
       {tipo: "nvc",  texto: "9. Necesidades de la congregación", mins: 7},
       {tipo: "extra",texto: "Palabras de conclusión", mins: 3},
       {tipo: "tk",   texto: "<b>Discurso de servicio</b>", mins: 30 }
@@ -50,7 +50,7 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Nada puede impedir que “el Dios de la verdad” cumpla su propósito", mins: 30},
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo mantener la perspectiva correcta ante las dificultades", mins: 30},
       {tipo: "tk",  texto: "<b>Discurso final</b>", mins: 30}
     ]
   },
