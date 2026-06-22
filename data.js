21f2cfaf7203ba6cb3d68ebbd835ec61c0d23bef
favioabrchavez <favioabrchavez@gmail.com>
2026-06-22 13:31:33 -0500

    Update program texts and add new sections

--- data.js
+++ data.js
@@ -4,25 +4,16 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "extra",texto: "Palabras de introducción", mins: 1},
-      {tipo: "tdlb", texto: "1. No respetaron el templo de Jehová", mins: 10},
+      {tipo: "tdlb", texto: "1. ¿De qué presumirá usted?", mins: 10},
       {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
       {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
-      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
-      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
-      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 4},
+      {tipo: "smm",  texto: "5. Empiece conversaciones", mins: 4},
+      {tipo: "smm",  texto: "6. Haga revisitas", mins: 4},
       {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
-      {tipo: "nvc",  texto: "7. ¿Cómo podemos demostrar que respetamos el Salón del Reino?", mins: 5},
-      {tipo: "nvc",  texto: "8. Cómo usamos las donaciones: Mantenemos nuestros Salones del Reino en buen estado", mins: 10},
-      {tipo: "nvc",  texto: "9. Estudio bíblico de la congregación", mins: 30},
+      {tipo: "nvc",  texto: "7. <i>No nos dejemos engañar, apoyemos el Reino de Dios (Jer. 10:23)</i>", mins: 5},
+      {tipo: "nvc",  texto: "8. Estudio bíblico de la congregación", mins: 30},
       {tipo: "extra",texto: "Palabras de conclusión", mins: 3}
-    ]
-  },
-
-  w: {
-    nombre: "",
-    programa: [
-      {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo mantener la perspectiva correcta ante las dificultades", mins: 60}
     ]
   },
 
@@ -30,26 +21,33 @@ const TEMPAD_DATA = {
     nombre: "",
     programa: [
       {tipo: "extra",texto: "Palabras de introducción", mins: 1},
-      {tipo: "tdlb", texto: "1. No respetaron el templo de Jehová", mins: 10},
+      {tipo: "tdlb", texto: "1. ¿De qué presumirá usted?", mins: 10},
       {tipo: "tdlb", texto: "2. Busquemos perlas escondidas", mins: 10},
       {tipo: "tdlb", texto: "3. Lectura de la Biblia", mins: 4},
-      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 3},
-      {tipo: "smm",  texto: "5. Haga revisitas", mins: 4},
-      {tipo: "smm",  texto: "6. Haga discípulos", mins: 5},
+      {tipo: "smm",  texto: "4. Empiece conversaciones", mins: 4},
+      {tipo: "smm",  texto: "5. Empiece conversaciones", mins: 4},
+      {tipo: "smm",  texto: "6. Haga revisitas", mins: 4},
       {tipo: "extra",texto: "Consejo para estudiantes", mins: 1},
-      {tipo: "nvc",  texto: "7. ¿Cómo podemos demostrar que respetamos el Salón del Reino?", mins: 5},
-      {tipo: "nvc",  texto: "8. Cómo usamos las donaciones: Mantenemos nuestros Salones del Reino en buen estado", mins: 10},
-      {tipo: "nvc",  texto: "9. Estudio bíblico de la congregación", mins: 30},
+      {tipo: "nvc",  texto: "7. <i>No nos dejemos engañar, apoyemos el Reino de Dios (Jer. 10:23)</i>", mins: 5},
+      {tipo: "nvc",  texto: "8. Estudio bíblico de la congregación", mins: 30},
       {tipo: "extra",texto: "Palabras de conclusión", mins: 3},
       {tipo: "tk",   texto: "<b>Discurso de servicio</b>", mins: 30 }
     ]
   },
 
+  w: {
+    nombre: "",
+    programa: [
+      {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Aprendamos del “Dios de todo consuelo”", mins: 60}
+    ]
+  },
+  
   w_sup: {
     nombre: "",
     programa: [
       {tipo: "tk",  texto: "<b>Discurso público</b>", mins: 30},
-      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Cómo mantener la perspectiva correcta ante las dificultades", mins: 30},
+      {tipo: "w",   texto: "<b>Estudio de <i>La Atalaya</i></b>: Aprendamos del “Dios de todo consuelo”", mins: 30},
       {tipo: "tk",  texto: "<b>Discurso final</b>", mins: 30}
     ]
   },
